0b0d1ae556ae0a7217fb1f5f177ec13a6cb79ee3
Diegodkid <diegodkid@gmail.com>
2023-03-28 21:05:44 -0400

    css-modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import { Header } from "./components/Header"
 import { Post } from "./Post"
 
 
 function App() {
   return (
     <div>
+      <Header />
       <Post 
         author='Diego' 
         content='Lorem ipsum dolor sit amet consectetur, adipisicing elit.'
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import style from './Header.module.css'
+
+export function Header() {
+  return (
+    <header className={style.header}>
+      <strong>Ignite Feed</strong>
+    </header>
+  )
+}
