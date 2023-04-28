cdad2bc45df61079ca995c093e027349922575a0
Diegodkid <diegodkid@gmail.com>
2023-04-28 06:36:57 -0400

    Entendendo a key

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -51,9 +51,9 @@ export function Post({ author, publishedAt, content }) {
     <div className={styles.content}>
       {content.map(line => {
         if (line.type === 'paragraph') {
-          return <p>{line.content}</p>
+          return <p key={line.content}>{line.content}</p>
         } else if (line.type === 'link') {
-          return <p><a href='#'>{line.content}</a></p>
+          return <p key={line.content}><a href='#'>{line.content}</a></p>
         }
       })}
       <p>
@@ -77,7 +77,7 @@ export function Post({ author, publishedAt, content }) {
     </form>
 
     <div className={styles.commentList}>
-      {comments.map(coment => <Comment content={coment} />)}
+      {comments.map(coment => <Comment key={coment} content={coment} />)}
     </div>
    </article>
   )
